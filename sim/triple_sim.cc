b89f48bbc0272e2f2ca11e50c5217878bc44b92c
Peter Dillinger <peterd@fb.com>
2020-01-28 15:53:59 -0800

    Simulate segments (fuse) or shards

--- sim/triple_sim.cc
+++ sim/triple_sim.cc
@@ -16,19 +16,23 @@ inline uint32_t fastrange32(uint32_t hash, uint32_t range) {
     return static_cast<uint32_t>(wide >> 32);
 }
 
-size_t r0_3(uint64_t h, size_t seg_len, size_t segs_base) {
+static size_t seg_len = 0;
+static size_t segs_base = 0;
+static bool segs_not_shards = false;
+
+size_t r0_3(uint64_t h) {
     size_t rv = (h % segs_base) * seg_len;
     h /= segs_base;
     return rv + (h % seg_len);
 }
-size_t r1_3(uint64_t h, size_t seg_len, size_t segs_base) {
-    size_t rv = ((h % segs_base) + 1) * seg_len;
+size_t r1_3(uint64_t h) {
+    size_t rv = ((h % segs_base) + (1 * segs_not_shards)) * seg_len;
     h /= segs_base;
     h /= seg_len;
     return rv + (h % seg_len);
 }
-size_t r2_3(uint64_t h, size_t seg_len, size_t segs_base) {
-    size_t rv = ((h % segs_base) + 2) * seg_len;
+size_t r2_3(uint64_t h) {
+    size_t rv = ((h % segs_base) + (2 * segs_not_shards)) * seg_len;
     h /= segs_base;
     h /= seg_len;
     h /= seg_len;
@@ -50,26 +54,39 @@ int main(int argc, char *argv[]) {
 
     size_t nkeys = (size_t)std::atoi(argv[1]);
     size_t len = (size_t)std::atoi(argv[2]);
-    size_t segs_base = (size_t)std::atoi(argv[3]);
+    int segs_or_shards = std::atoi(argv[3]);
+    if (segs_or_shards < 0) {
+        // negative -> shards  (-1 -> Xor filter)
+        segs_base = (size_t)-segs_or_shards;
+        segs_not_shards = false;
+        seg_len = len / segs_base;
+        len = seg_len * segs_base;
+    } else if (segs_or_shards > 0) {
+        // positive -> segments
+        segs_base = (size_t)segs_or_shards;
+        segs_not_shards = true;
+        seg_len = len / (segs_base + 2);
+        len = seg_len * (segs_base + 2);
+    } else {
+        return 1;
+    }
 
     std::vector<uint64_t> *arr = new std::vector<uint64_t>[len];
 
     size_t collision2 = 0;
     size_t collision3 = 0;
     size_t good_collision = 0;
-    size_t seg_len = len / (segs_base + 2);
-    len = seg_len * (segs_base + 2);
     uint64_t mod = seg_len * seg_len * seg_len * segs_base;
     for (size_t i = 0; i < nkeys; ++i) {
         uint64_t h = (uint64_t)rand() % mod;
-        size_t h0 = r0_3(h, seg_len, segs_base);
+        size_t h0 = r0_3(h);
         if (std::find(arr[h0].begin(), arr[h0].end(), h) != arr[h0].end()) {
             good_collision++;
         } else {
             arr[h0].push_back(h);
-            size_t h1 = r1_3(h, seg_len, segs_base);
+            size_t h1 = r1_3(h);
             arr[h1].push_back(h);
-            size_t h2 = r2_3(h, seg_len, segs_base);
+            size_t h2 = r2_3(h);
             arr[h2].push_back(h);
             if (h0 == h1 || h1 == h2 || h0 == h2) {
                 collision3++;
@@ -107,7 +124,7 @@ int main(int argc, char *argv[]) {
                     later_mapped++;
                 }
                 uint64_t h = arr[i][0];
-                for (size_t j : {r0_3(h, seg_len, segs_base), r1_3(h, seg_len, segs_base), r2_3(h, seg_len, segs_base)}) {
+                for (size_t j : {r0_3(h), r1_3(h), r2_3(h)}) {
                     remove(arr[j], h);
                 }
             } else {
@@ -121,7 +138,7 @@ int main(int argc, char *argv[]) {
                 if (count == 2) {
                     kicked++;
                     uint64_t h = arr[i][0];
-                    for (size_t j : {r0_3(h, seg_len, segs_base), r1_3(h, seg_len, segs_base), r2_3(h, seg_len, segs_base)}) {
+                    for (size_t j : {r0_3(h), r1_3(h), r2_3(h)}) {
                         remove(arr[j], h);
                     }
                     good_kick = true;
@@ -134,7 +151,7 @@ int main(int argc, char *argv[]) {
                     if (count > 1) {
                         kicked++;
                         uint64_t h = arr[i][0];
-                        for (size_t j : {r0_3(h, seg_len, segs_base), r1_3(h, seg_len, segs_base), r2_3(h, seg_len, segs_base)}) {
+                        for (size_t j : {r0_3(h), r1_3(h), r2_3(h)}) {
                             remove(arr[j], h);
                         }
                         break;
